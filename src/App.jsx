580758050ad54d97595300e9843471c63cc61b98
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-04-11 21:03:36 +0700

    Comment topbar, sidebar, and home in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Topbar />
+      {/* <Topbar />
       <main>
         <Sidebar />
         <Home />
-      </main>
+      </main> */}
     </div>
   );
 }
