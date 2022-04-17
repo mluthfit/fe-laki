5c0cdfa8460f5d72e86a223061c40ca5bf65308d
ge-wijayanto <greg.evan01@gmail.com>
2022-04-18 01:31:49 +0700

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import Topbar from "./components/Topbar";
-import Sidebar from "./components/Sidebar";
-import Home from "./components/Home";
+// import Sidebar from "./components/Sidebar";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Topbar />
-      <main>
-        <Sidebar />
-        <Home />
-      </main>
+    <div>
+        <Topbar />
+        <Login />
     </div>
   );
 }
