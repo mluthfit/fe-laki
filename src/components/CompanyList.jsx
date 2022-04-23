268b808ed1ba4fa9c65557be571f48dd3d23be67
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-04-23 17:50:39 +0700

    Add company list's table UI and functional and edit left

--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -1,12 +1,236 @@
-import React from "react";
+import React, { useState } from "react";
+import DataTable from "react-data-table-component";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faLink, faEye, faEdit } from "@fortawesome/free-solid-svg-icons";
 import "./css/company-list.css";
 
 const CompanyList = () => {
+  const [image, setImage] = useState("https://via.placeholder.com/151");
+  const onPopupImage = (event) => {
+    event.stopPropagation();
+    const logoPopup = document.querySelector(".logo-popup");
+    if (!logoPopup) return;
+
+    logoPopup.classList.toggle("open");
+  };
+
   const onHandleSubmit = (event) => {
     event.preventDefault();
   };
+
+  const columns = [
+    {
+      name: "#",
+      cell: (_, index) => index + 1,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Address",
+      selector: (row) => row.address,
+      sortable: true,
+      grow: 2,
+    },
+    {
+      name: "Phone",
+      selector: (row) => row.phone,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+      sortable: true,
+    },
+    {
+      name: "Web",
+      cell: (row) => (
+        <a
+          href={row.website}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="web-button"
+        >
+          <FontAwesomeIcon icon={faLink}></FontAwesomeIcon>
+        </a>
+      ),
+      grow: 0,
+    },
+    {
+      name: "Logo",
+      cell: (row) => (
+        <span
+          className="logo-button"
+          onClick={(e) => {
+            setImage(row.logo);
+            onPopupImage(e);
+            console.log(image);
+          }}
+        >
+          <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+        </span>
+      ),
+      grow: 0,
+    },
+    {
+      name: "Action",
+      cell: (row) => (
+        <span className="edit-button">
+          <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+        </span>
+      ),
+      grow: 0,
+    },
+  ];
+
+  const data = [
+    {
+      id: 1,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 2,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 3,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/150",
+    },
+    {
+      id: 4,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 5,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/150",
+    },
+    {
+      id: 6,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 7,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/150",
+    },
+    {
+      id: 8,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 9,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/150",
+    },
+    {
+      id: 10,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 11,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/150",
+    },
+    {
+      id: 12,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 13,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/150",
+    },
+    {
+      id: 14,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+    {
+      id: 15,
+      name: "Teslah Crop.",
+      address: "JL. Lorem Ipsum",
+      phone: "+62 82345679876",
+      email: "noreply@teslah.com",
+      website: "https://www.google.com",
+      logo: "https://via.placeholder.com/150",
+    },
+    {
+      id: 16,
+      name: "Super Inc.",
+      address: "JL. Jend. Sudirman",
+      phone: "+62 82323487654",
+      email: "admin@super.co.id",
+      website: "https://www.facebook.com",
+      logo: "https://via.placeholder.com/151",
+    },
+  ];
 
   return (
     <div className="company-list">
@@ -76,18 +300,18 @@ const CompanyList = () => {
         </form>
       </div>
       <div className="company-table">
-        <table>
+        {/* <table>
           <tr>
             <th>No.</th>
             <th>Name</th>
-            <th>Addhess</th>
-            <th>Phone Numbeh</th>
+            <th>Address</th>
+            <th>Phone Number</th>
             <th>Email</th>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>Web</th>
+            <th>Logo</th>
+            <th>Action</th>
           </tr>
-          <tr>
+          <tr className="tbody">
             <td>1</td>
             <td>Teslad Crop</td>
             <td>Jl. Jendral Sudirman</td>
@@ -96,11 +320,40 @@ const CompanyList = () => {
             <td>
               <FontAwesomeIcon icon={faLink}></FontAwesomeIcon>
             </td>
-            <td>Edit</td>
-            <td>Details</td>
+            <td>
+              <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+            </td>
+            <td>
+              <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+            </td>
           </tr>
-        </table>
+          <tr className="tbody">
+            <td>1</td>
+            <td>Teslad Crop</td>
+            <td>Jl. Jendral Sudirman</td>
+            <td>+62 82390128123</td>
+            <td>admin@super.co.id</td>
+            <td>
+              <FontAwesomeIcon icon={faLink}></FontAwesomeIcon>
+            </td>
+            <td>
+              <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+            </td>
+            <td>
+              <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+            </td>
+          </tr>
+        </table> */}
+        <h2 className="title">Company List</h2>
+        <DataTable columns={columns} data={data} pagination></DataTable>
       </div>
+      <div className="logo-popup">
+        <div className="background-popup" onClick={onPopupImage}></div>
+        <div className="popup-box">
+          <img src={image} alt="Company's logo" />
+        </div>
+      </div>
+      <span onClick={onPopupImage}>Click here</span>
     </div>
   );
 };
